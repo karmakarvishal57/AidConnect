cee4b2779a5796b1510501d22ff572c047edba8d
unknown <karmakarvishal57@gmail.com>
2026-06-10 22:37:53 +0530

    preview updated in vite config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +10,8 @@ export default defineConfig({
     extend: {
       colors: {},
     },
+  },
+  preview: {
+    allowedHosts: ['host.docker.internal'],
   },
 });
